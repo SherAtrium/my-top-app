04dd3760a9d307276e3f27b9ef0a099b5dcb5ad4
Sherzod <sherzodm214@gmail.com>
2021-12-09 12:33:29 +0500

    Created HeadTitle component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,24 @@
-import {AppProps} from 'next/dist/shared/lib/router/router';
+import { AppProps } from 'next/dist/shared/lib/router/router';
 
 import '../styles/globals.css';
-import Head from "next/head";
+import Head from 'next/head';
 
-function MyApp({Component, pageProps}: AppProps): JSX.Element {
-	return (
-			<>
-				<Head>
-					<title>MyTop - App</title>
-					<link rel="icon" href="/favicon.ico"/>
-				</Head>
-				<Component {...pageProps} />
-			</>
-	);
+function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+  return (
+    <>
+      <Head>
+        <title>MyTop - Мой лучший топ</title>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
