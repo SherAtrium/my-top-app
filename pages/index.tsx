013f583e6deb5ef67d2cbc7b11ff81e458c34808
Sherzod <sherzodm214@gmail.com>
2021-12-09 13:20:29 +0500

    Added classnames packet and created Button component with styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,12 @@
+import { Button } from '../components/Button/Button';
 import { HeadlineTag } from '../components/HeadlineTag/HeadlineTag';
 
 export default function Home(): JSX.Element {
   return (
     <>
-      <HeadlineTag tag="h1">Head Title</HeadlineTag>
-      <HeadlineTag tag="h2">Head Title</HeadlineTag>
-      <HeadlineTag tag="h3">Head Title</HeadlineTag>
+      <HeadlineTag tag='h1'>Текст</HeadlineTag>
+      <Button appearance='primary'>Кнопка</Button>
+      <Button appearance='ghost'>Кнопка</Button>
     </>
   );
 }
