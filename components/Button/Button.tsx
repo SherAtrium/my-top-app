53ced4ba9c15f2b2f970162023e964b00ab77d20
Sherzod <sherzodm214@gmail.com>
2021-12-12 11:03:17 +0500

    Added svg reader to webpack component, added arrow in button component

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,17 +1,35 @@
 import cn from 'classnames';
 import { ButtonProps } from './Button.props';
 
+import ArrowIcon from './arrow.svg';
 import Styles from './Button.module.css';
 
-export const Button = ({ children, appearance }: ButtonProps): JSX.Element => {
+export const Button = ({
+  arrow = 'none',
+  appearance,
+  className,
+  children,
+  ...props
+}: ButtonProps): JSX.Element => {
   return (
     <button
-      className={cn(Styles.button, {
+      className={cn(Styles.button, className, {
         [Styles.primary]: appearance === 'primary',
         [Styles.ghost]: appearance === 'ghost',
       })}
+      {...props}
     >
       {children}
+      {arrow !== 'none' && (
+        <span
+          className={cn(Styles.arrow, {
+            [Styles.down]: arrow === 'down',
+            [Styles.right]: arrow === 'right',
+          })}
+        >
+          <ArrowIcon />
+        </span>
+      )}
     </button>
   );
 };
