dc39d2306c5f0c3a70d300b817a38e68bf99be29
Sherzod <sherzodm214@gmail.com>
2021-12-12 12:34:40 +0500

    Added Paragraph component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import { Button } from '../components/Button/Button';
 import { HeadlineTag } from '../components/HeadlineTag/HeadlineTag';
+import { Paragraph } from '../components/Paragraph/Paragraph';
 
 export default function Home(): JSX.Element {
   return (
@@ -11,6 +12,9 @@ export default function Home(): JSX.Element {
       <Button appearance='ghost' arrow='right'>
         Кнопка
       </Button>
+      <Paragraph size='l'>Большой</Paragraph>
+      <Paragraph>Средний</Paragraph>
+      <Paragraph size='s'>Маленький</Paragraph>
     </>
   );
 }
