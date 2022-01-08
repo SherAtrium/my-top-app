3b7e1a2898f910e8adea9464bb1551a2443a0c23
Sherzod <sherzodm214@gmail.com>
2022-01-08 10:28:58 +0500

    big updates

--- layout/Menu/Menu.tsx
+++ layout/Menu/Menu.tsx
@@ -1,45 +1,14 @@
 import { useContext } from 'react';
+import { useRouter } from 'next/dist/client/router';
 import { AppContext } from '../../context/app.context';
-import { TopLevelCategory } from '../../interfaces/page.interface';
-import { FirstLevelMenuItem, PageItem } from '../../interfaces/menu.interface';
 
 import cn from 'classnames';
 import Link from 'next/link';
-import { useRouter } from 'next/dist/client/router';
 
-import CoursesIcon from './icons/courses.svg';
-import ServicesIcon from './icons/services.svg';
-import BooksIcon from './icons/books.svg';
-import ProductsIcon from './icons/products.svg';
+import { firstLevelMenu } from '../../helpers/helpers';
+import { FirstLevelMenuItem, PageItem } from '../../interfaces/menu.interface';
 
 import Styles from './Menu.module.css';
-
-const firstLevelMenu: FirstLevelMenuItem[] = [
-  {
-    route: 'courses',
-    name: 'Курсы',
-    icon: <CoursesIcon />,
-    id: TopLevelCategory.Courses,
-  },
-  {
-    route: 'services',
-    name: 'Сервисы',
-    icon: <ServicesIcon />,
-    id: TopLevelCategory.Services,
-  },
-  {
-    route: 'books',
-    name: 'Книги',
-    icon: <BooksIcon />,
-    id: TopLevelCategory.Books,
-  },
-  {
-    route: 'products',
-    name: 'Продукты',
-    icon: <ProductsIcon />,
-    id: TopLevelCategory.Products,
-  },
-];
 
 export const Menu = (): JSX.Element => {
   const { menu, setMenu, firstCategory } = useContext(AppContext);
