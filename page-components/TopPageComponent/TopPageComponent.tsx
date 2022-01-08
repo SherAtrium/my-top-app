2e6beffaf0b2e077c1703473ce41a9313b97deb0
Sherzod <sherzodm214@gmail.com>
2022-01-08 17:25:03 +0500

    created Card, HhData Components

--- page-components/TopPageComponent/TopPageComponent.tsx
+++ page-components/TopPageComponent/TopPageComponent.tsx
@@ -1,9 +1,42 @@
+import { Tag } from '../../components/Tag/Tag';
 import { TopPageComponentProps } from './TopPageComponent.props';
+import { HeadlineTag } from '../../components/HeadlineTag/HeadlineTag';
+
+import Styles from './TopPageComponent.module.css';
+import { Card } from '../../components/Card/Card';
+import { HhData } from '../../components/HhData/HhData';
 
 export const TopPageComponent = ({
   page,
   products,
   firstCategory,
 }: TopPageComponentProps): JSX.Element => {
-  return <>{products && products.length}</>;
+  return (
+    <div className={Styles.wrapper}>
+      <div className={Styles.title}>
+        <HeadlineTag tag='h1'>{page.title}</HeadlineTag>
+        {products && (
+          <Tag color='gray' size='m'>
+            {products.length}
+          </Tag>
+        )}
+        <span>Сортировка</span>
+      </div>
+
+      <div>
+        {products && products.map((p) => <div key={p._id}>{p.title}</div>)}
+      </div>
+
+      <div className={Styles.hhTitle}>
+        <HeadlineTag tag='h2'>Вакансии - {page.category}</HeadlineTag>
+        {products && (
+          <Tag color='red' size='m'>
+            hh.ru
+          </Tag>
+        )}
+      </div>
+
+      <HhData {...page.hh} />
+    </div>
+  );
 };
