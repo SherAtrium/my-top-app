db0f4fd007df4f2c04edb341b129fb7930d43d59
Sherzod <sherzodm214@gmail.com>
2022-01-08 22:07:24 +0500

    Cards Styled

--- components/HhData/HhData.tsx
+++ components/HhData/HhData.tsx
@@ -1,10 +1,15 @@
 import { HhDataProps } from './HhData.props';
-import cn from 'classnames';
-
-import Styles from './hhData.module.css';
 import { Card } from '../Card/Card';
 
-export const HhData = ({ count }: HhDataProps): JSX.Element => {
+import RateIcon from './rate.svg';
+import Styles from './HhData.module.css';
+
+export const HhData = ({
+  count,
+  juniorSalary,
+  middleSalary,
+  seniorSalary,
+}: HhDataProps): JSX.Element => {
   return (
     <div className={Styles.hh}>
       <Card className={Styles.count}>
@@ -12,9 +17,36 @@ export const HhData = ({ count }: HhDataProps): JSX.Element => {
         <div className={Styles.countValue}>{count}</div>
       </Card>
 
-      <Card className={Styles.count}>
-        <div className={Styles.title}>Всего вакансии</div>
-        <div className={Styles.countValue}>{count}</div>
+      <Card className={Styles.salary}>
+        <div>
+          <div className={Styles.title}>Начальный</div>
+          <div className={Styles.salaryValue}>{juniorSalary}</div>
+          <div className={Styles.rate}>
+            <RateIcon className={Styles.filled} />
+            <RateIcon />
+            <RateIcon />
+          </div>
+        </div>
+
+        <div>
+          <div className={Styles.title}>Средний</div>
+          <div className={Styles.salaryValue}>{middleSalary}</div>
+          <div className={Styles.rate}>
+            <RateIcon className={Styles.filled} />
+            <RateIcon className={Styles.filled} />
+            <RateIcon />
+          </div>
+        </div>
+
+        <div>
+          <div className={Styles.title}>Профессионал</div>
+          <div className={Styles.salaryValue}>{seniorSalary}</div>
+          <div className={Styles.rate}>
+            <RateIcon className={Styles.filled} />
+            <RateIcon className={Styles.filled} />
+            <RateIcon className={Styles.filled} />
+          </div>
+        </div>
       </Card>
     </div>
   );
